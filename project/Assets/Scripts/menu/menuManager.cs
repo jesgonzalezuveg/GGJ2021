f7bf8381828d6a6d3d57d74e266baa261c1e2c63
QuebinM <mgkadrian13@gmail.com>
2021-01-29 19:06:28 -0600

    menuFinal

--- project/Assets/Scripts/menu/menuManager.cs
+++ project/Assets/Scripts/menu/menuManager.cs
@@ -7,9 +7,12 @@ using UnityEngine.SceneManagement;
 public class menuManager : MonoBehaviour
 {
     public string cambioEscena;
+
     public GameObject transicion;
     public GameObject creditos;
     public GameObject botonesMenu;
+
+    public Animator animatorPlayer;
 
     private void Awake() {
         transicion.gameObject.SetActive(false);
@@ -21,23 +24,31 @@ public class menuManager : MonoBehaviour
         transicion.gameObject.SetActive(true);
         StartCoroutine(cargarNivelEnum());
     }
+
     public void irAMenu(){
-        creditos.SetActive(false);
-        botonesMenu.SetActive(true);  
+        animatorPlayer.SetBool("isBacking", true);
+        StartCoroutine(regresar());
     }
+
     public void irCreditos()
     {
         creditos.SetActive(true);
         botonesMenu.SetActive(false);
     }
+
     public void quitBtn()
     {
         Application.Quit();
     }
 
-
     IEnumerator cargarNivelEnum(){
         yield return new WaitForSeconds(5);
         SceneManager.LoadSceneAsync(cambioEscena);
+    }
+    IEnumerator regresar(){
+        yield return new WaitForSeconds(1);
+        
+        creditos.SetActive(false);
+        botonesMenu.SetActive(true);  
     }
 }
